004b9fc0a99e0a2ad9ef2783521caeb559a94c36
p-bray <pbr23869@gmail.com>
2024-09-28 21:39:09 -0400

    Not sure why the @click directive isn't working. Committing for the night tho

--- start/index.js
+++ start/index.js
@@ -27,7 +27,32 @@ const app = Vue.createApp({
                     seatsAvailable: 2,
                     earlyBird: false
                 }
-            ]
+            ],
+            product: {
+                name: 'Cruise to the moon',
+                description: 'Cruise to the moon in our luxurious shuttle. Watch the astronauts working outside the International Space Station.',
+                image: {
+                    src: 'assets/cruise.jpg',
+                    description: 'An astronaut floats outside the window while you sit in comfort',
+                    style: { 'border-radius': '15px' },
+                },
+                cabins: [
+                    { name: 'Coach', price: 125000 },
+                    { name: 'Business', price: 275000 },
+                    { name: 'First', price: 430000 },
+                ]
+            },
+            booking: {
+                cabinIndex: 0,
+                notes: '',
+                completed: false
+            }
         }
     },
+    methods: {
+        bookCabin() {
+            this.booking.completed = true;
+            console.log(this.booking.completed);
+        }
+    }
 });
